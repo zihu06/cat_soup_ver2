fc480cb8d05b1262fadf9519432514cf41ab4a99
최지후\zihu1 <zihu1028@gmail.com>
2025-06-09 17:58:12 +0900

    돌발퀘스트 삭제 되었어서 다시 업로드

--- cat_soup_ver2/cat_soup_ver2.c
+++ cat_soup_ver2/cat_soup_ver2.c
@@ -8,6 +8,21 @@
 #define BWL_PO (ROOM_WIDTH -2)
 
 int main() {
+	int soup = 0; //수프의 개수
+	int CP = 0; // CP 포인트
+	int feel = 3; // 쫀떡이 기분
+	int feel_tmp = 0; // 쫀떡이 기분 임시저장
+	int relationship = 2;// 친밀도
+	int cat = 1;// 고양이
+	int dice;//주사위
+	int foot; // 발자국
+	int choose = 5; // 선택지
+	int turn = 0; // 턴 수
+	int s = 0; // 스크래처 위치
+	int tower = 0; // 캣타워 위치
+	int up, down; // 거리 비교 변수
+	int answer = 0; // 돌발퀘스트 정답
+	srand((unsigned int)time(NULL));
 
 	printf("****야옹이와 수프****\n");
 	printf("         /\\_/\\\n");
@@ -17,34 +32,19 @@ int main() {
 	printf("     |  \\|| ||\n");
 	printf("     \\ ' || ||\n");
 	printf("      \\) ()-())\n");
-
 	printf("야옹이의 이름은 쫀떡입니다\n");
 
 	Sleep(500); // 2.5초 대기
 	system("cls");
 
-	int soup = 0; //수프의 개수
-	int cp = 0; // CP 포인트
-	int feel = 3; // 쫀떡이 기분
-	int feel_tmp = 0; // 쫀떡이 기분 임시저장
-	int relationship = 2;// 친밀도
-	int cat = 1;// 고양이
-	int dice;//주사위
-	int foot; // 발자국
-	int choose ;
-	int turn = 0;
-	int s = 0; // 스크래처 위치
-	int tower = 0; // 타워 위치
-	int up, down; // 가까운 거리 판별하는 변수
 	
-
-	srand((unsigned int)time(NULL));
+	
 
 	while (1) {
 		turn++;
 		printf("==================== 현재상태===================\n");
 		printf("현재까지 만든 수프: %d개\n", soup);
-		printf("CP: %d포인트\n", cp);
+		printf("CP: %d포인트\n", CP);
 		printf("쫀떡이 기분(0~3): %d\n", feel);
 		if (feel == 0) {
 			printf("기분이 매우 나쁩니다.\n");
@@ -79,26 +79,25 @@ int main() {
 
 		printf("================================================\n");
 		
-		if (turn== 3) {
-			printf("돌발 퀘스트 발생!\n");
+		if (turn == 3) {
+			printf("돌발퀘스트 발생!\n");
 			printf("12 X 12 = ?\n");
-			printf("'?'에 들어올숫자를 적으시오.");
-			scanf_s("%d", &choose);
-			
-			if (choose == 144) {
-				printf("정답입니다.\n");
-				printf("CP가 5올라갑니다.\n");
-				cp += 5;
+			printf("'?'에 들어올 수를 넣으세요\n");
+			scanf("%d", &answer);
+			if (answer == 144) {
+				printf("정답입니다! CP가 10포인트 증가합니다.\n");
+				CP += 10;
 			}
-			else
-				printf("틀렸습니다.\n정답은 144입니다.\n");
-
+			else {
+				printf("틀렸습니다. CP가 5포인트 감소합니다.\n");
+				if (CP >= 5) {
+					CP -= 5;
+				}
+				else {
+					CP = 0;
+				}
+			}
 		}
-
-
-
-
-
 
 
 		printf("6-%d: 주사위 눈이 %d 이하이면 그냥 기분이 나빠집니다. \n", relationship, 6 - relationship);
@@ -118,14 +117,12 @@ int main() {
 
 		}
 
-
-		if (feel == 0) {
-			printf("기분이 매우 나쁜 지후은(는) 집으로 향합니다.\n");
-			if (cat > 1) {
-				cat--;
-			}
-		}
-		else if (feel == 1) {
+		switch (feel) {
+		case 0:
+			printf("기분이 매우나쁜 지후는 집으로향합니다.\n");
+			cat--;
+			break;
+		case 1:
 			if (s != 0 && tower != 0) {
 				if (cat != s && cat != tower) {
 					if (cat - s > 0 && cat - tower > 0)
@@ -147,14 +144,14 @@ int main() {
 						up = up - cat;
 						down = cat - down;
 
-						if (up < down) {
+						if (up > down) {
 							cat++;
 							if (choose == 1)
 								printf("지후은(는) 심심해서 타워쪽으로 이동합니다.\n");
 							else
 								printf("지후은(는) 심심해서 스크래처쪽으로 이동합니다.\n");
 						}
-						else if (down < up) {
+						else if (down > up) {
 							cat--;
 							if (choose == 0)
 								printf("지후은(는) 심심해서 타워쪽으로 이동합니다.\n");
@@ -188,17 +185,19 @@ int main() {
 				if (feel > 0)
 					feel--;
 			}
+			break;
+		case 2:
+			printf("지후은(는) 기분좋게 식빵을 굽고있습니다.\n");
+			break;
+		case 3:
+			printf("%s은(는) 골골송을 부르며 수프를 만들러갑니다.\n");
+			cat++;
+			break;
 		}
-		else if (feel == 2) {
-			printf("지후는 기분좋게 식빵을 굽고 있습니다.\n");
-		}
-		else if (feel == 3) {
-			printf("지후는 골골송을 부르며 수프를 만들러 갑니다.\n");
-			if (cat < BWL_PO) {
-				cat++;
-			}
-		}
+
 		
+
+
 
 			if (cat == BWL_PO) {
 				soup++;
@@ -206,13 +205,13 @@ int main() {
 				switch (dice) {
 
 				case 1:
-					printf("지후가 감자수프를 만들었습니다.\n");
+					printf("쫀떡이 감자수프를 만들었습니다.\n");
 					break;
 				case 2:
-					printf("지후가 양송이수프를 만들었습니다.\n");
+					printf("쫀떡이 양송이수프를 만들었습니다.\n");
 					break;
 				case 3:
-					printf("지후가 브로콜리수프를 만들었습니다.\n");
+					printf("쫀떡이 브로콜리수프를 만들었습니다.\n");
 					break;
 				}
 				printf("현재까지 만든 수프: %d\n", soup);
